1b91a1bfe0abf82c847c5f50584295a92428f013
umer-k-abdullah <umerabdullahwork1@gmail.com>
2024-07-02 18:35:28 +0500

    main page is designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import LandingPage from "./modules/LandingPage/Pages/LandingPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-screen h-screen">
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index path="/" component={<LandingPage />} />
+          {/* <Route path="/another" component={AnotherPage} /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import LandingPage from "../modules/LandingPage/Pages/LandingPage";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+function MainLayout() {
+  return (
+    <>
+      <div className="relative w-full h-screen flex ">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover object-center"
+          autoPlay
+          muted
+          loop
+        >
+          <source
+            src="../assets/videos/banner-background-video.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <div className="absolute inset-0 bg-gradient-to-t from-[#01E37580] via-[#010101b7] to-[#010101] opacity-100"></div>
+        {/* <div className="relative z-10 flex flex-col items-center justify-center w-full h-full">
+          <Navbar/>
+          <Sidebar/>
+          <LandingPage />
+        </div> */}
+        <div className="relative z-10 flex flex-col items-center justify-center w-full h-full">
+          <div className="w-full max-w-screen-lg mx-auto">
+            <Navbar />
+          </div>
+          <Sidebar />
+          <LandingPage />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MainLayout;
