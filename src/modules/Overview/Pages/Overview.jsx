6abcdf8e1af38248a4cecd95cc3eb3fdeb0e755f
Shaheem707 <muhammadshaheem77@gmail.com>
2024-07-11 19:11:50 +0500

    Dashboard completed with dummy data as per desgin

--- src/modules/Overview/Pages/Overview.jsx
+++ src/modules/Overview/Pages/Overview.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import TopCard from "../Components/TopCard";
 import StatsCard from "../Components/StatsCard";
+import { useNavigate } from "react-router-dom";
+
 
 function Overview() {
+
+  const navigate = useNavigate()
   const statsLabels = [
     "Opponent",
     "Time Stamp",
@@ -29,13 +33,33 @@ function Overview() {
       },
     },
   ];
-  
+
+  const match2Stats = [
+    {
+      timeStamp: {
+        time: "4 Jul, 24",
+        date: "17:35",
+      },
+      matchStatus: "Lost",
+      battleTime: "14m 17s",
+      skillScore: {
+        inc_dec: "-289",
+        score: "6545",
+      },
+      totalProfit: {
+        inc_dec: "-100K",
+        profit: "188M",
+      },
+    },
+  ];
+
   return (
-    <div className="h-full w-full pt-20 pl-36 gap-5 flex flex-col">
+    <div className="h-full w-full pt-20 pl-32 gap-3 flex flex-col">
       <div className="mx-auto w-[75%]">
         <h1 className="font-zen-dots text-white text-3xl">DASHBOARD</h1>
+        <h3 className="text-white text-lg mt-5">Profile Stats</h3>
       </div>
-      <div className="mx-auto flex w-[75%] gap-6 h-1/2">
+      <div className="mx-auto font-poppins flex w-[75%] gap-6 h-1/3">
         <TopCard
           icon={"/assets/icons/win-streak.png"}
           label={"Skill Score"}
@@ -54,8 +78,12 @@ function Overview() {
           inc_dec={"+12%"}
         />
       </div>
-      <div className="flex flex-col gap-2 mx-auto w-[75%] my-3 h-1/2">
-        <div className="flex w-full justify-between items-start">
+      <div className="flex flex-col gap-2 mx-auto w-[75%] my-3 h-1/2 font-poppins">
+        <div className="flex items-center justify-between">
+          <h3 className="text-white text-lg">Previous Matches</h3>
+          <div className="text-themeGreen cursor-pointer hover:underline" onClick={()=>{navigate("/history-log")}}>View All</div>
+        </div>
+        <div className="flex w-full justify-between pl-20">
           {statsLabels.map((elem) => (
             <span className="text-[#EDF1FAB2] text-base">{elem}</span>
           ))}
@@ -70,16 +98,7 @@ function Overview() {
               totalProfit={ele.totalProfit}
             />
           ))}
-          {match1Stats.map((ele) => (
-            <StatsCard
-              timeStamp={ele.timeStamp}
-              matchStatus={ele.matchStatus}
-              battleTime={ele.battleTime}
-              skillScore={ele.skillScore}
-              totalProfit={ele.totalProfit}
-            />
-          ))}
-          {match1Stats.map((ele) => (
+          {match2Stats.map((ele) => (
             <StatsCard
               timeStamp={ele.timeStamp}
               matchStatus={ele.matchStatus}
