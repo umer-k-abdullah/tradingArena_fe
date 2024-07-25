ca61bbf961bee69fe225024a49ed7a91860aa303
Shaheem707 <muhammadshaheem77@gmail.com>
2024-07-25 18:30:02 +0500

    Skeleton for Profile Page developed

--- src/modules/Profile/Pages/Profile.jsx
+++ src/modules/Profile/Pages/Profile.jsx
@@ -82,6 +82,63 @@ function Profile() {
     </div>
   );
 
+  const testing = false;
+  const ProfileSkeleton = () => (
+    <div className="h-full w-full mt-20 pt-2 pl-32 gap-7 flex flex-col overflow-y-scroll custom-scrollbar">
+      <div className="mx-auto w-[75%] flex justify-between items-center">
+        <div className="h-7 w-48 rounded-xl bg-[#1a1a1a]"></div>
+        <button className="bg-themeGreen font-poppins h-9 w-20 rounded-md font-medium form-btn-shadow">
+        </button>
+      </div>
+      <div className="mx-auto font-poppins flex flex-col w-[75%] gap-6 h-full">
+        <div className="bg-[#0d0d0d] w-full flex h-48 items-start p-8 gap-8 rounded-xl ">
+          <div className="h-32 w-32 rounded-full bg-[#1a1a1a]"></div>
+          <div className=" flex-1 h-36 flex flex-col justify-start gap-3">
+            <div className="flex justify-between items-center">
+              <div className="flex gap-2 h-4 rounded-full w-28 bg-[#1a1a1a]"></div>
+              <div className="h-4 rounded-full w-24 bg-[#1a1a1a]"></div>
+            </div>
+            <div className="h-5 rounded-full w-24 bg-[#1a1a1a]"></div>
+            <div className="h-32 rounded-xl w-full bg-[#1a1a1a]"></div>
+          </div>
+        </div>
+        <div className="grid grid-cols-5 w-full gap-5">
+          {miniStatsCardData.map(() => (
+            <div className="bg-[#0D0D0D] rounded-lg  h-28 flex flex-col justify-center items-center gap-3">
+              <div className="h-5 w-32 rounded-xl bg-[#1a1a1a]"></div>
+              <div className="h-5 w-10 rounded-xl bg-[#1a1a1a]"></div>
+            </div>
+          ))}
+        </div>
+        <div className="bg-[#0D0D0D] w-full h-[400px] p-6 rounded-xl mb-4 flex flex-col gap-3">
+          <div className="flex">
+            <div className="flex-1">
+              
+              <div className="h-10 w-44 rounded-xl bg-[#1a1a1a]"></div>
+            </div>
+            <div className="flex flex-1 justify-between">
+              <div className="flex items-center gap-4">
+                <div className="h-2 w-2 bg-[#1a1a1a] rounded-full"></div>
+                <div className="h-4 w-16 rounded-full bg-[#1a1a1a]"></div>
+                <div className="h-2 w-2 bg-[#1a1a1a] rounded-full"></div>
+                <div className="h-4 w-16 rounded-full bg-[#1a1a1a]"></div>
+              </div>
+              <div>
+                <div className="w-24 h-10 bg-[#1a1a1a] rounded-xl"></div>
+              </div>
+            </div>
+          </div>
+          <div className="h-[290px] w-full bg-[#1a1a1a] rounded-xl">
+            
+          </div>
+        </div>
+        <div className="w-full h-[70px]">
+          <p>.</p>
+        </div>
+      </div>
+    </div>
+  );
+
   const defaultProfile = {
     profileImage: "assets/images/avatar1.png",
     firstName: "Adam",
@@ -199,7 +256,10 @@ function Profile() {
       show: false,
     },
   };
-
+  // Configure the Skeleton here for API calls
+  if (testing) {
+    return <ProfileSkeleton />;
+  }
   return (
     <div className="h-full w-full mt-20 pt-2 pl-32 gap-7 flex flex-col overflow-y-scroll custom-scrollbar">
       <div className="mx-auto w-[75%] flex justify-between">
